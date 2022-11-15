78dcc4d0f44ecb063f69006df35a67b0c639a928
opacteam <mike@minisisinc.com>
2022-11-15 02:36:49 -0800

    fix port

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const template = require('./api');
 app.use(express.json({ extended: false }));
 app.use('/api', template);
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`server started on port ${PORT}`);
+    console.log(`server started on port`);
 });
